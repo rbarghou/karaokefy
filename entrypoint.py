25573799241c72f087d6aecf40a3682548a821d5
Ramsey Barghouti <ramsey.barghouti@gmail.com>
2024-03-11 14:37:36 -0400

    added simple storage hello world

--- entrypoint.py
+++ entrypoint.py
@@ -1,3 +1,10 @@
 #! /usr/local/bin/python
 
-print("HERE")
+from google.cloud import storage
+
+bucket_name = "karaoke-songs-for-lam-presentation"
+storage_client = storage.Client()
+bucket = storage_client.get_bucket(bucket_name)
+blob = bucket.blob('file-name')
+with blob.open("w") as f:
+    f.write('Hello, World!')
