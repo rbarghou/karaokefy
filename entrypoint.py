1aa396dfab3883387175e80b814fe27a1577163a
Ramsey Barghouti <ramsey.barghouti@gmail.com>
2024-03-11 15:36:56 -0400

    use env variables.

--- entrypoint.py
+++ entrypoint.py
@@ -1,14 +1,13 @@
 #! /usr/local/bin/python
 
-import sys
+import os
 from google.cloud import storage
 
 bucket_name = "karaoke-songs-for-lam-presentation"
 storage_client = storage.Client()
 bucket = storage_client.get_bucket(bucket_name)
 
-print(sys.argv)
-
+print(f"{(os.environ.get('FOO'), os.environ.get('BAR'))=}")
 
 # blob = bucket.blob('file-name')
 # with blob.open("w") as f:
