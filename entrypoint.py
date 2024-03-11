170c72cbd0e27409be11122bee36b2c8d8f1e012
Ramsey Barghouti <ramsey.barghouti@gmail.com>
2024-03-11 15:23:38 -0400

    read command line args

--- entrypoint.py
+++ entrypoint.py
@@ -1,10 +1,15 @@
 #! /usr/local/bin/python
 
+import sys
 from google.cloud import storage
 
 bucket_name = "karaoke-songs-for-lam-presentation"
 storage_client = storage.Client()
 bucket = storage_client.get_bucket(bucket_name)
-blob = bucket.blob('file-name')
-with blob.open("w") as f:
-    f.write('Hello, World!')
+
+print(sys.argv)
+
+
+# blob = bucket.blob('file-name')
+# with blob.open("w") as f:
+#     f.write('Hello, World!')
