7f5ac9a8ca69aa0ced17df2eeb02f158c04ed41b
Ramsey Barghouti <ramsey.barghouti@gmail.com>
2024-03-13 14:34:06 -0400

    added a little more UI

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   const [data, setData] = useState({});
+  const [youtubeUrl, setYoutubeUrl] = useState("");
 
   useEffect(() => {
     axios.get("https://us-central1-lam-presentation.cloudfunctions.net/get_song_list")
@@ -14,18 +15,35 @@ function App() {
         .catch(error => {
             console.log(error)
         })
-  }, [])
+  }, []);
+
+  const handleYoutubeUrlChange = (event) => {
+    const { value } = event.target;
+    setYoutubeUrl(value);
+  };
+
+  const handleYoutubeUrlSubmit = (event) => {
+    event.preventDefault();
+    console.log(youtubeUrl);
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
         <div>
+          <form>
+            <label>Song Request YouTube URL: </label>
+            <input type="text" name="YoutubeURL" onChange={handleYoutubeUrlChange}/>
+            <input type="submit" value="Submit" onClick={handleYoutubeUrlSubmit}/>
+          </form>
           <ol>
             {Object.entries(data).map(([key, value]) => (
               <li>{key}
                 <ul>
                   <li>status: {value.status}</li>
-                  <li><a href={value.song_link}>Karaoke Version song link</a></li>
-                  <li><a href={value.YoutubeURL}>Original YoutubeLink</a></li>
+                  <li><a href={value.song_link} target="_blank" rel="noreferrer">Karaoke Version song link</a></li>
+                  <li><a href={value.YoutubeURL} target="_blank" rel="noreferrer">Original YoutubeLink</a></li>
                 </ul>
               </li>
             ))}
