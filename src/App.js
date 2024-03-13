1d88cfda03f76c952c02a4a1313f2574168b80fe
Ramsey Barghouti <ramsey.barghouti@gmail.com>
2024-03-13 14:06:49 -0400

    some _really_ basic ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios.get("https://us-central1-lam-presentation.cloudfunctions.net/get_song_list")
+        .then(response => {
+            setData(response.data);
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.log(error)
+        })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <ol>
+            {Object.entries(data).map(([key, value]) => (
+              <li>{key}
+                <ul>
+                  <li>status: {value.status}</li>
+                  <li><a href={value.song_link}>song link</a></li>
+                </ul>
+              </li>
+            ))}
+          </ol>
+        </div>
       </header>
     </div>
   );
